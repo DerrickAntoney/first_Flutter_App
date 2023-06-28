846b2507fe77073a240e61c88693f4feb886ce79
Derrick Antoney <derrickantoney@gmail.com>
2023-06-28 11:01:15 +0300

    Added buttons and quiz texts

--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,45 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  var questionIndex = 0;
+  void answerQuestion() {
+    questionIndex = questionIndex + 1;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold(
-      appBar: AppBar(title: const Text('My First App'),
-    ),
-      body: const Text('This is the default text'),
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?'
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Text(
+              questions[questionIndex]
+            ),
+            TextButton(
+              onPressed: answerQuestion,
+              child: Text('Answer 1'),
+            ),
+            TextButton(
+              onPressed: () => print("Answer 2 chosen!"),// Anonymous fn
+              child: Text('Answer 2'),
+            ),
+            TextButton(
+              onPressed: () {   //Anonymous fn
+                //...
+                print('Answer 3 chosen!');
+              },
+              child: const Text('Answer 3'),
+            ),
+          ],
+        ),
       ),
-    ) ;
+    );
   }
 }
